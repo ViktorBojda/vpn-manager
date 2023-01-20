6172427e9c1bf1d15a8ad0f1fcf5b59e0a52b1a4
Viktor Bojda <viktorbojda17@gmail.com>
2023-01-20 13:55:07 +0100

    Add func for adding new users and for rebuilding org containers by id

--- pritunl_api/static/pritunl_api/users_organizations.js
+++ pritunl_api/static/pritunl_api/users_organizations.js
@@ -2,7 +2,7 @@ let urlBase = "/pritunl/api/";
 let orgData = [];
 let userData = {};
 
-$("#bttn-add-org").on("click", function () {
+$("#btn-add-org").on("click", function () {
     $("#modal-header").text("Add Group");
     $("#modal-body").html(
         `<div class="mb-3">
@@ -14,13 +14,41 @@ $("#bttn-add-org").on("click", function () {
     $("#modal").modal("show");
 });
 
-$("#bttn-add-user").on("click", function () {
+function submitAddUser() {
+    let formData = $("form").serializeArray();
+    let orgID;
+    let user = {};
+    
+    formData.forEach(function(item, idx, object) {
+        if (item.value.trim().length === 0)
+            object.splice(idx, 1);
+        else
+            user[item.name] = item.value; 
+    });
+    orgID = user.organization;
+    delete user.organization;
+
+    $.ajax({
+        method: "POST",
+        url: urlBase + "organizations/" + orgID + "/users/create/",
+        data: user
+    }).done(function() {
+        $("#modal").modal("hide");
+        $.when(reloadUsersByOrgID(orgID)).then(function() {
+            rebuildOrgByID(orgID);
+        });
+    }).fail(function(xhr) {
+        alert(xhr.responseText);
+    })
+}
+
+$("#btn-add-user").on("click", function () {
     if (!orgData.length) {
         console.error("No organizations found, you must add organization before you can add user!");
         return;
     }
     let orgSelect = $(
-        `<select id='form-input-org' class='form-select' required>
+        `<select id='form-input-org' name='organization' class='form-select' required>
             <option value="" selected disabled>Select organization</option>
         </select>`
     );
@@ -29,27 +57,44 @@ $("#bttn-add-user").on("click", function () {
         orgSelect.append($(`<option value=${val.id}>${val.name}</option>`));
     });
 
-
     $("#modal-header").text("Add User");
     $("#modal-body").html(
         `<div class="mb-3">
             <label for="form-input-name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="form-input-name" required>
+            <input type="text" class="form-control" id="form-input-name" name="name" required>
         </div>
         <div class="mb-3">
             <label for="form-input-org" class="form-label">Organization</label>
             ${orgSelect.prop("outerHTML")}
         </div>
         <div class="mb-3">
+            <label for="form-input-group" class="form-label">Groups</label>
+            <input type="text" class="form-control" id="form-input-group" name="groups">
+        </div>
+        <div class="mb-3">
             <label for="form-input-email" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="form-input-email">
+            <input type="email" class="form-control" id="form-input-email" name="email">
         </div>`
     )
+    $("form").off().on("submit", function(event) {
+        event.preventDefault();
+        submitAddUser();
+    });
 
     $("#modal").modal("show");
 });
 
-function fillOrgContainer() {
+function rebuildOrgByID(orgID) {
+    let userList = $("#org-" + orgID + " ul");
+    userList.empty();
+    if (orgID in userData) {
+        userData[orgID].forEach(user => {
+            userList.append($("<li class='list-group-item'>" + user.name + "</li>"));
+        });
+    }
+}
+
+function rebuildOrgContainer() {
     let orgContainer = $("#org-container");
     orgContainer.empty();
 
@@ -97,7 +142,7 @@ function reloadAllData() {
         orgData = data;
         userData = {};
         $.when.apply($, reloadAllUsers()).then(function () {
-            fillOrgContainer()
+            rebuildOrgContainer()
         });
     });
 }
