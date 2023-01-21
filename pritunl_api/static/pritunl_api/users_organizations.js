0b9f0e6633c660e8e736a12361d7f1dafc12b85a
Viktor Bojda <viktorbojda17@gmail.com>
2023-01-21 19:31:31 +0100

    Add sorting for created users and organizations

--- pritunl_api/static/pritunl_api/users_organizations.js
+++ pritunl_api/static/pritunl_api/users_organizations.js
@@ -2,14 +2,78 @@ let urlBase = "/pritunl/api/";
 let orgData = [];
 let userData = {};
 
+function sortOrgs() {
+    let orgs = $("#org-container .card").get();
+    if (!orgs.length) {
+        console.error("Failed to sort, no organizations found.");
+        return;
+    }
+
+    orgs.sort(function (org1, org2) {
+        return $(org1).children(".card-header").text().trim().localeCompare($(org2).children(".card-header").text().trim())
+    })
+
+    $("#org-container").append(orgs);
+}
+
+function sortUserList(orgID) {
+    let users = $("#org-" + orgID + " .list-group-item").get();
+    if (!users.length) {
+        console.error("Failed to sort, no users found under organization with ID (" + orgID + ").");
+        return;
+    }
+
+    users.sort(function (user1, user2) {
+        return $(user1).text().trim().localeCompare($(user2).text().trim())
+    })
+
+    $("#org-" + orgID + " .list-group").append(users);
+}
+
+function submitAddOrg() {
+    let formData = $("form").serializeArray();
+    let org = {};
+
+    formData.forEach(function (item, idx, object) {
+        if (item.value.trim().length === 0)
+            object.splice(idx, 1);
+        else
+            org[item.name] = item.value;
+    });
+
+    $.ajax({
+        method: "POST",
+        url: urlBase + "organizations/create/",
+        data: org
+    }).done(function (response) {
+        $("#modal").modal("hide");
+        $.when(reloadOrgs()).then(function () {
+            $("#org-container").append($(
+                `<div id=org-${response.id} class='card my-3'>
+                    <div class='card-header'>${response.name}</div>
+                    <ul class='list-group list-group-flush'>
+                </div>`
+            ))
+            sortOrgs();
+        });
+    }).fail(function (xhr) {
+        alert(xhr.responseText);
+    })
+}
+
 $("#btn-add-org").on("click", function () {
     $("#modal-header").text("Add Group");
-    $("#modal-body").html(
+    $("#form").html(
         `<div class="mb-3">
             <label for="form-input-name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="form-input-name" required>
+            <input type="text" class="form-control" id="form-input-name" name="name" required>
         </div>`
     )
+
+    $("form").off().on("submit", function (event) {
+        event.preventDefault();
+        submitAddOrg();
+    });
 
     $("#modal").modal("show");
 });
@@ -18,12 +82,12 @@ function submitAddUser() {
     let formData = $("form").serializeArray();
     let orgID;
     let user = {};
-    
-    formData.forEach(function(item, idx, object) {
+
+    formData.forEach(function (item, idx, object) {
         if (item.value.trim().length === 0)
             object.splice(idx, 1);
         else
-            user[item.name] = item.value; 
+            user[item.name] = item.value;
     });
     orgID = user.organization;
     delete user.organization;
@@ -32,12 +96,14 @@ function submitAddUser() {
         method: "POST",
         url: urlBase + "organizations/" + orgID + "/users/create/",
         data: user
-    }).done(function() {
+    }).done(function (response) {
         $("#modal").modal("hide");
-        $.when(reloadUsersByOrgID(orgID)).then(function() {
-            rebuildOrgByID(orgID);
+        $.when(reloadUsersByOrgID(orgID)).then(function () {
+            // rebuildUserListByOrgID(orgID);
+            $("#org-" + orgID + " ul").append($("<li class='list-group-item'>" + response[0].name + "</li>"));
+            sortUserList(orgID);
         });
-    }).fail(function(xhr) {
+    }).fail(function (xhr) {
         alert(xhr.responseText);
     })
 }
@@ -58,7 +124,7 @@ $("#btn-add-user").on("click", function () {
     });
 
     $("#modal-header").text("Add User");
-    $("#modal-body").html(
+    $("#form").html(
         `<div class="mb-3">
             <label for="form-input-name" class="form-label">Name</label>
             <input type="text" class="form-control" id="form-input-name" name="name" required>
@@ -76,7 +142,8 @@ $("#btn-add-user").on("click", function () {
             <input type="email" class="form-control" id="form-input-email" name="email">
         </div>`
     )
-    $("form").off().on("submit", function(event) {
+
+    $("form").off().on("submit", function (event) {
         event.preventDefault();
         submitAddUser();
     });
@@ -84,15 +151,29 @@ $("#btn-add-user").on("click", function () {
     $("#modal").modal("show");
 });
 
-function rebuildOrgByID(orgID) {
-    let userList = $("#org-" + orgID + " ul");
-    userList.empty();
-    if (orgID in userData) {
-        userData[orgID].forEach(user => {
-            userList.append($("<li class='list-group-item'>" + user.name + "</li>"));
-        });
-    }
-}
+// function rebuildOrgByID(orgID) {
+//     let orgInfo = orgData.filter(obj => obj.id == orgID);
+//     if (!orgInfo.length) {
+//         console.error("There is no organization with given ID, failed to rebuild!");
+//         return;
+//     }
+//     let orgHeader = $("#org-" + orgID + " .card-header");
+//     if (orgHeader.length)
+//         orgHeader.text(orgInfo[0].name);
+//     else {
+
+//     }
+// }
+
+// function rebuildUserListByOrgID(orgID) {
+//     let userList = $("#org-" + orgID + " ul");
+//     userList.empty();
+//     if (orgID in userData) {
+//         userData[orgID].forEach(user => {
+//             userList.append($("<li class='list-group-item'>" + user.name + "</li>"));
+//         });
+//     }
+// }
 
 function rebuildOrgContainer() {
     let orgContainer = $("#org-container");
@@ -134,17 +215,22 @@ function reloadAllUsers() {
     return ajaxCalls;
 }
 
-function reloadAllData() {
-    $.when($.ajax({
+function reloadOrgs() {
+    return $.ajax({
         type: "GET",
         url: urlBase + "organizations/"
-    })).done(function (data) {
+    }).done(function (data) {
         orgData = data;
+    });
+}
+
+function reloadAllData() {
+    $.when(reloadOrgs()).then(function () {
         userData = {};
         $.when.apply($, reloadAllUsers()).then(function () {
             rebuildOrgContainer()
         });
-    });
+    })
 }
 
 $(window).on('load', function () {
